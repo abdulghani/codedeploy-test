17a7fb6ec55d8cfa15715cb6ae5f5878a547d8fa
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-11 13:05:04 +0700

    fix: require ssh key

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,6 +3,9 @@
 echo ""
 echo "DEPLOYING"
 
+eval "$(ssh-agent)"
+ssh-add ~/.ssh/stage_key
+
 scp "$(pwd)/docker-compose.yml" "$REMOTE_USER@$REMOTE_HOST:~/app/docker-compose.yml"
 scp "$(pwd)/docker_image.tar" "$REMOTE_USER@$REMOTE_HOST:~/app/docker_image.tar"
 ssh "$REMOTE_USER@$REMOTE_HOST" "bash -s" < "$(pwd)/.scripts/deploy-remote-command.sh"
