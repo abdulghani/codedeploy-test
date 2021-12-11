d0325fa45861c341ff93ba7b61129a9be9b69aa4
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-11 12:07:25 +0700

    fix: use ssh location

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "DEPLOYING, CONNECTING TO ($REMOTE_USER) AT ($REMOTE_HOST)"
-# ASSUMING SSH KEY ALREADY INPUT
+echo "DEPLOYING"
+eval "$(ssh-agent)"
+ssh-add ~/.ssh/stage_key
 
-# scp "$(pwd)/docker-compose.yml" "$REMOTE_USER@$REMOTE_HOST:~/app/docker-compose.yml"
-
-# echo "UPLOADING DOCKER IMAGE..."
-# scp "$(pwd)/docker_image.tar" "$REMOTE_USER@$REMOTE_HOST:~/app/docker_image.tar"
+scp "$(pwd)/docker-compose.yml" "$REMOTE_USER@$REMOTE_HOST:~/app/docker-compose.yml"
+scp "$(pwd)/docker_image.tar" "$REMOTE_USER@$REMOTE_HOST:~/app/docker_image.tar"
 ssh "$REMOTE_USER@$REMOTE_HOST" "bash -s" < "$(pwd)/.scripts/deploy-remote-command.sh"
