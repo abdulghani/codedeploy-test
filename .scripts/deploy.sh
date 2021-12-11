9ec08701e748f09a4a57af2098f5f407aa5e0125
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-11 12:16:10 +0700

    fix: comment out ssh command

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -6,4 +6,6 @@ ssh-add ~/.ssh/stage_key
 
 scp "$(pwd)/docker-compose.yml" "$REMOTE_USER@$REMOTE_HOST:~/app/docker-compose.yml"
 scp "$(pwd)/docker_image.tar" "$REMOTE_USER@$REMOTE_HOST:~/app/docker_image.tar"
-ssh "$REMOTE_USER@$REMOTE_HOST" "bash -s" < "$(pwd)/.scripts/deploy-remote-command.sh"
+
+echo "DONE"
+# ssh "$REMOTE_USER@$REMOTE_HOST" "bash -s" < "$(pwd)/.scripts/deploy-remote-command.sh"
