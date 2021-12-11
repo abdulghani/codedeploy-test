f3b49adcbe321a8639e645d936efd06a955b3c98
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-11 15:52:23 +0700

    fix: move docker cleanup command to cleanup script

--- .scripts/application-start.sh
+++ .scripts/application-start.sh
@@ -6,4 +6,3 @@ cd ~/app
 
 echo "STARTING UP DOCKER COMPOSE"
 sudo docker-compose -f ./docker-compose.yml up -d --force-recreate
-sudo docker system prune --force
--- .scripts/cleanup.sh
+++ .scripts/cleanup.sh
@@ -3,3 +3,4 @@
 # set permission to write
 sudo chown -R ubuntu:ubuntu ~/app
 [ -d ~/app ] && rm -rf ~/app
+sudo docker system prune --force
