9de4d7f5a9fc1034a6bbc782bed0e9845997de01
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-11 17:36:45 +0700

    fix: update changes

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 
 const indexHandler: RequestHandler = async (req, res) => {
-  const name = req.query.name + "!" ?? "world";
+  const name = req.query.name ?? "world";
   return res.status(200).send({
     message: `hello ${name}`,
     param: req.query,
