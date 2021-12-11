83cd4293f730437ea4b7f37ae18367face7283d8
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-11 12:03:15 +0700

    fix: remove scp command

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -3,8 +3,8 @@
 echo "DEPLOYING, CONNECTING TO ($REMOTE_USER) AT ($REMOTE_HOST)"
 # ASSUMING SSH KEY ALREADY INPUT
 
-scp "$(pwd)/docker-compose.yml" "$REMOTE_USER@$REMOTE_HOST:~/app/docker-compose.yml"
+# scp "$(pwd)/docker-compose.yml" "$REMOTE_USER@$REMOTE_HOST:~/app/docker-compose.yml"
 
-echo "UPLOADING DOCKER IMAGE..."
-scp "$(pwd)/docker_image.tar" "$REMOTE_USER@$REMOTE_HOST:~/app/docker_image.tar"
+# echo "UPLOADING DOCKER IMAGE..."
+# scp "$(pwd)/docker_image.tar" "$REMOTE_USER@$REMOTE_HOST:~/app/docker_image.tar"
 ssh "$REMOTE_USER@$REMOTE_HOST" "bash -s" < "$(pwd)/.scripts/deploy-remote-command.sh"
