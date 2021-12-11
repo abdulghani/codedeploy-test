1c45d28884d71475c5c9d307fb9076db84d71b66
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-11 17:27:48 +0700

    fix: add update

--- src/endpoints/ping.ts
+++ src/endpoints/ping.ts
@@ -3,6 +3,7 @@ import { RequestHandler } from "express";
 const pingHandler: RequestHandler = async (req, res) => {
   res.status(200).send({
     message: "pong",
+    query: Object.keys(req.query).length > 0 ? req.query : undefined,
   });
 };
 
