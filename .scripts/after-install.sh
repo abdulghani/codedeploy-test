3008bb4e88a78908949cb8e4d9f057feb8ac96f3
Abdul Ghani <contact.abdulghani@gmail.com>
2021-12-31 09:25:38 +0700

    fix: update cleanup downloaded docker image

--- .scripts/after-install.sh
+++ .scripts/after-install.sh
@@ -4,7 +4,10 @@ DOCKER_IMAGE=$(aws secretsmanager get-secret-value --secret-id fe-stage-deploy-i
 DWN_DOCKER_IMAGE="downloaded_docker_image.tar"
 
 echo "DOWNLOADING DOCKER IMAGE ($DOCKER_IMAGE)..."
-aws s3 cp $DOCKER_IMAGE ~/app/$DWN_DOCKER_IMAGE
+aws s3 cp $DOCKER_IMAGE ~/$DWN_DOCKER_IMAGE
 
 echo "LOADING DOCKER IMAGE..."
-docker load -i ~/app/$DWN_DOCKER_IMAGE
+docker load -i ~/$DWN_DOCKER_IMAGE
+
+echo "CLEANUP DOWNLOADED DOCKER IMAGE..."
+rm -rf ~/$DWN_DOCKER_IMAGE
