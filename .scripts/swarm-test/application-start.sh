4f6b83e25c82f68a6f8a22721bd58b1fed90480a
Abdul Ghani <contact.abdulghani@gmail.com>
2022-01-09 21:58:37 +0700

    fix: await for stack remove to be done

--- .scripts/swarm-test/application-start.sh
+++ .scripts/swarm-test/application-start.sh
@@ -11,6 +11,6 @@ IS_SWARM_LEADER=$(docker node inspect self | jq -r ".[0].ManagerStatus.Leader")
 
 if [ $IS_SWARM_LEADER = 'true' ]; then
     cd ~/project
-    docker stack rm "$STACK_NAME" &&
+    docker stack rm "$STACK_NAME" && sleep 5
     docker stack deploy --compose-file ./docker-compose.yml "$STACK_NAME"
 fi
