b207ea9c172cfb874017dfc5dc6c20dbba9c5832
Abdul Ghani <contact.abdulghani@gmail.com>
2022-01-09 22:13:07 +0700

    fix: add update

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -4,7 +4,7 @@ const indexHandler: RequestHandler = async (req, res) => {
   const name = req.query.name ?? "there";
   return res.status(200).send({
     message: `hello ${name}. updated change`,
-    param: Object.keys(req.query).length > 0 ? req.query : undefined,
+    query: req.query,
   });
 };
 
