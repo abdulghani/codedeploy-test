577b45a0a23019378a05a59fd116799dff8df7e6
Abdul Ghani <contact.abdulghani@gmail.com>
2022-01-09 22:09:47 +0700

    fix: add recursive function to restart stack

--- .scripts/swarm-test/application-start.sh
+++ .scripts/swarm-test/application-start.sh
@@ -9,8 +9,18 @@ docker node update --availability active $NODE_ID
 # RESTART SERVICE IF NODE IS MANAGER
 IS_SWARM_LEADER=$(docker node inspect self | jq -r ".[0].ManagerStatus.Leader")
 
+IS_RESTARTED=false
+create_stack() {
+    sleep 3 && 
+    docker stack deploy --compose-file ./docker-compose.yml "$STACK_NAME" && IS_RESTARTED=true || IS_RESTARTED=false
+
+    if [ $IS_RESTARTED = false ]; then
+        create_stack
+    fi
+}
+
 if [ $IS_SWARM_LEADER = 'true' ]; then
     cd ~/project
-    docker stack rm "$STACK_NAME" && sleep 5
-    docker stack deploy --compose-file ./docker-compose.yml "$STACK_NAME"
+    docker stack rm "$STACK_NAME"
+    create_stack
 fi
