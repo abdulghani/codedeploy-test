27f9f1a2bdcb169793bde1cf3bc2d3ff8e514c3b
Abdul Ghani <contact.abdulghani@gmail.com>
2022-01-09 21:21:45 +0700

    fix: update script

--- .scripts/swarm-test/application-stop.sh
+++ .scripts/swarm-test/application-stop.sh
@@ -10,7 +10,7 @@ SECRETS_ID="swarm-test-join-manager"
 if [ $IS_SWARM_LEADER = 'true' ]; then
     # update secrets manager join command
     JOIN_COMMAND=$(docker swarm join-token manager | grep -i -Po '(?!\s+)docker swarm join --token .*(?!\s+$)')
-    aws secretsmanager update-secret --secret-id $SECRETS_ID --secret-string $JOIN_COMMAND
+    aws secretsmanager update-secret --secret-id $SECRETS_ID --secret-string "$JOIN_COMMAND"
 else
     # join swarm cluster
     JOIN_COMMAND=$(aws secretsmanager get-secret-value --secret-id $SECRETS_ID | jq -r '.SecretString')
